51e4818bf4db08c80fe99466f5a1dc430fdd644d
ydylh <643962011@qq.com>
2022-04-17 12:15:01 +0800

    feature: 修改样式

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState, colorRef } from 'react';
 import { Graph, Addon } from '@antv/x6';
 import './compnents/registerNode';
 import { nodeTypes } from './compnents/nodeType';
 import { bindEvents } from './compnents/graphEvents';
 import { grapgConfig } from './compnents/graphConfig';
+import SelectColorView from './compnents/selectColor';
 import { Button } from 'antd';
 import html2canvas from 'html2canvas';
 import './App.scss';
@@ -12,6 +13,16 @@ function App() {
   const [graph, setGraph] = useState(null);
   const [stencil, setStencil] = useState(null);
   const [isLoadingBtn, setIsLoadingBtn] = useState(false);
+  const [bgc, setBgc] = useState(''); // 节点填充
+  const [transparent, setTransparent] = useState(1); // 透明度调整
+  const [colorVisible, setColorVisible] = useState(false); // 填充弹窗
+  const [beforeVal, setBeforeVal] = useState('');
+  const [type, setType] = useState(''); // 设置修改文字/填充类型
+  const [txtColor, setTxtColor] = useState('#333'); // 设置文字颜色
+  const [txtVisible, setTxtVisible] = useState(false); // 文本颜色弹窗
+  const [isSelectModal, setIsSelectModal] = useState(false); // 选择样式弹窗
+  const [selectedNode, setSelectNode] = useState({});
+  let colorRef = useRef();
 
   useEffect(() => {
     const graph = new Graph(grapgConfig('graph_container'))
@@ -39,8 +50,14 @@ function App() {
     setStencil(stencil);
     // 绑定事件
     bindEvents(graph);
+    graph.on('node:click', ({ node }) => {
+      setSelectNode(node);
+      setIsSelectModal(true);
+      setBgc(node?.attrs?.body?.fill || '#5F95FF');
+      setTransparent(node?.attrs?.body?.opacity);
+      setTxtColor(node?.attrs?.text.fill)
+    });
   }, []);
-
 
   // 绑定节点
   nodeTypes(graph, stencil);
@@ -65,11 +82,112 @@ function App() {
     setIsLoadingBtn(false);
   }
 
+  // 确定颜色
+  const sureColor = () => {
+    setBgc(colorRef?.current?.getFieldValue('color') || '#fff');
+  };
+
+  // 选择颜色
+  const selectedColor = (color) => {
+    if (colorVisible) {
+      setBgc(color);
+    }
+    if (txtVisible) {
+      setTxtColor(color)
+    }
+  }
+
+  // 改变透明度
+  const onChangeTransparent = (val) => {
+    if (colorVisible) {
+      setTransparent(val);
+    }
+  }
+
+  // 打开颜色弹窗
+  const openColorVisoble = (cusType) => {
+    setType(cusType);
+    switch (cusType) {
+      case 'fill':
+        setColorVisible(true);
+        setBeforeVal(bgc);
+        break;
+      case 'text':
+        setTxtVisible(true);
+        setBeforeVal(txtColor);
+        break;
+      default:
+        break;
+    }
+  };
+
+  // 取消打开弹窗
+  const cancelTooTip = () => {
+    switch (type) {
+      case 'fill':
+        setColorVisible(false);
+        setBgc(beforeVal);
+        break;
+      case 'text':
+        setTxtVisible(false);
+        setTxtColor(beforeVal);
+        break;
+      default:
+        break;
+    }
+  };
+
+  // 确定弹窗
+  const sureTooTip = () => {
+    switch (type) {
+      case 'fill':
+        setColorVisible(false)
+        break;
+      case 'text':
+        setTxtVisible(false);
+        break;
+      default:
+        break;
+    }
+  };
+
+  // 确定选择样式弹窗
+  const sureSelectModal = () => {
+    selectedNode.attr('body/fill', bgc);
+    selectedNode.attr('body/opacity', transparent); // 改变背景透明度
+    selectedNode.attr('text/fill', txtColor);
+    selectedNode.attr('text/fontSize', 12); // 改变字体大小
+    setIsSelectModal(false);
+  };
+
+  // 取消选择样式弹窗
+  const cancelSelectModal = () => {
+    setIsSelectModal(false)
+  };
 
   return (
     <div className="app">
       <div className='graph_container' id="graph_container" />
       <div id='graph_stencil' className='stencil' />
+      {isSelectModal && <div className='graph_controlPanel'>
+        <SelectColorView
+          sureColor={sureColor}
+          bgc={bgc}
+          type={type}
+          colorRef={colorRef}
+          transparent={transparent}
+          selectedColor={selectedColor}
+          colorVisible={colorVisible}
+          openColorVisoble={openColorVisoble}
+          onChangeTransparent={onChangeTransparent}
+          cancelTooTip={cancelTooTip}
+          sureTooTip={sureTooTip}
+          txtColor={txtColor}
+          txtVisible={txtVisible}
+          cancelSelectModal={cancelSelectModal}
+          sureSelectModal={sureSelectModal}
+        />
+      </div>}
       <Button
         type='primary'
         className='graph_save'
---
+++ src/compnents/selectColor.jsx
@@ -0,0 +1,99 @@
+import React, { Fragment, useState } from 'react';
+import { Input, Form, Slider, Button } from 'antd';
+import { CheckOutlined } from '@ant-design/icons';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+const oftenColorList = ['#000000', '#434343', '#cccccc', '#d9d9d9', '#ffffff', '#980000', '#ff0000', '#ff9900',
+  '#ffff00', '#00ff00', '#00ffff', '#4a86e8', '#0000ff', '#9900ff', '#ff00ff', '#e6b8af',
+  '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#c9daf8', '#cfe2f3', '#d9d2e9',
+  '#ead1dc', '#dd7e6b', '#ea9999', '#f9cb9c', '#ffe599', '#b6d7a8', '#a2c4c9', '#a4c2f4',
+  '#9fc5e8', '#b4a7d6', '#d5a6bd', '#cc4125', '#e06666', '#f6b26b', '#ffd966', '#93c47d',
+  '#76a5af', '#6d9eeb', '#6fa8dc', '#8e7cc3', '#c27ba0', '#a61c00', '#cc0000', '#e69138',
+  '#45818e', '#3c78d8', '#3d85c6', '#674ea7', '#a64d79', '#1c4587', '#073763', '#20124d',];
+
+function SelectColorView(props) {
+  const { bgc, sureColor, colorRef, selectedColor, onChangeTransparent,
+    transparent, openColorVisoble, colorVisible, cancelTooTip, sureTooTip,
+    type, txtColor, txtVisible, cancelSelectModal, sureSelectModal } = props;
+
+  const commonView = <Fragment>
+    {type === 'fill' && <div className='selectColor_silder'>
+      <Slider value={transparent} min={0} onChange={onChangeTransparent} max={1} step={0.01} />
+    </div>}
+    <div className='selectColor_list'>
+      {oftenColorList.map((item, index) => {
+        return <span onClick={() => selectedColor(item)} key={index} style={{ background: item }} className='selectColor_list-item' />
+      })}
+    </div>
+    <div style={{ textAlign: 'center' }}>
+      <Button size='small' onClick={cancelTooTip} style={{ marginRight: '10px' }}>取消</Button>
+      <Button size='small' type="primary" onClick={sureTooTip}>确定</Button>
+    </div>
+  </Fragment>
+
+  return <div className='selectColor'>
+    <div>
+      <Fragment>
+        <div className='selectColor-title'>填充颜色选择</div>
+        <div className='selectColor_styles'>
+          <span onClick={() => openColorVisoble('fill')} className='selectColor-showColor' style={{ backgroundColor: bgc, opacity: transparent }} />
+          <Form ref={colorRef}>
+            <Form.Item
+              name="color"
+            >
+              <Input
+                className='selectColor-selectSty'
+                size='small'
+                placeholder='请输入颜色'
+                addonAfter={
+                  <CheckOutlined onClick={sureColor} className='selectColor-icon' />
+                }
+              />
+            </Form.Item>
+          </Form>
+          {colorVisible && <div className='selectColor_styles_modal'>
+            {commonView}
+          </div>}
+        </div>
+      </Fragment>
+      <div className="selectColor_text">
+        <div className='selectColor-title'>文本样式选择</div>
+        <div className="selectColor_text-example">
+          <span style={{ marginRight: '5px', color: txtColor }}>示例</span>
+          <span onClick={() => openColorVisoble('text')} className='selectColor-showColor' style={{ backgroundColor: txtColor }} />
+          {txtVisible && <div className='selectColor_styles_modal'>
+            {commonView}
+          </div>}
+        </div>
+      </div>
+    </div>
+    <div style={{ textAlign: 'center' }}>
+      <Button size='small' onClick={cancelSelectModal} style={{ marginRight: '10px' }}>取消</Button>
+      <Button size='small' type="primary" onClick={sureSelectModal}>确定</Button>
+    </div>
+  </div >;
+}
+
+SelectColorView.defaultProps = {
+  bgc: '#c3c3c3',
+  sureColor: () => { },
+  selectedColor: () => { },
+  onChangeTransparent: () => { },
+  transparent: 1,
+  sureTooTip: () => { },
+  cancelTooTip: () => { },
+  type: 'fill',
+  txtColor: '#333',
+  colorVisible: false,
+  txtVisible: false,
+  cancelSelectModal: () => { },
+  sureSelectModal: () => { },
+};
+
+SelectColorView.propTypes = {
+  bgc: PropTypes.string,
+  sureColor: PropTypes.func,
+}
+
+export default SelectColorView;
